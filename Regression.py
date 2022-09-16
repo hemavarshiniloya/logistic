5b6a33799d5c5d2fb7b5d6da5733838f7f116f7b
hemavarshiniloya <112843810+hemavarshiniloya@users.noreply.github.com>
2022-09-16 11:29:27 +0530

    Create Regression.py

---
+++ Regression.py
@@ -0,0 +1,22 @@
+%%writefile app.py
+import streamlit as st
+import pickel
+pickel_in=open('Classifier.pkl','rb')
+clf=pickel.load(pickel_in)
+a=st.number_input('sepal lengthI(cm)')
+b=st.number_input('sepal width(cm)')
+c=st.number_input('sepal length(cm)')
+d=st.number_input('sepal width(cm)')
+r=' '
+if st.button('PREDICT'):
+    result=clf.predict([[a,b,.c,d]]).squeeze()
+    if result==0:
+        st.success('SETOSA')
+    elif result==0:
+        st.success('VERSCOLOR')
+    else:
+        st.success('VIRGINCA')
+
+
+
+
