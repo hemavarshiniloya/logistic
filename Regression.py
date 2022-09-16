896b706488f77f50cc908b8ecc32d81f368f5891
hemavarshiniloya <112843810+hemavarshiniloya@users.noreply.github.com>
2022-09-16 12:24:33 +0530

    Update Regression.py

--- Regression.py
+++ Regression.py
@@ -5,8 +5,8 @@ pickle_in=open('Classifier.pkl','rb')
 clf=pickle.load(pickle_in)
 a=st.number_input('sepal length')
 b=st.number_input('sepal width')
-c=st.number_input('sepal length')
-d=st.number_input('sepal width')
+c=st.number_input('petal length')
+d=st.number_input('petal width')
 result=' '
 if st.button('PREDICT'):
     result=clf.predict([[a,b,c,d]]).squeeze()
